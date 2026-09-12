92780008b9862c89dea41a08590ff72c6608168e
elianzozias3D <e.a.zozias@gmail.com>
2026-09-12 13:31:05 -0300

    Adicionando image target personalizado
    Criei uma pasta assets para inserir conteúdos da oficina e coloquei uma imagem png com a marca da petrobras para ser uma nova imagem alvo e ativar a experiência em realidade aumentada.

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const onxrloaded = () => {
       require('../image-targets/bmo-bites.json'),
       require('../image-targets/toggle-slam.json'),
       require('../image-targets/waves.json'),
+      require('../image-targets/target_petrobras.json'),
     ],
   })
   XR8.addCameraPipelineModule(LandingPage.pipelineModule())
