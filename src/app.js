8ad7b9cd452d66a9b007f402a9e1cc70abf8e338
elianzozias3D <e.a.zozias@gmail.com>
2026-09-25 17:34:21 -0300

    adições feitas na oficina ebat (25/09)

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const onxrloaded = () => {
       require('../image-targets/waves.json'),
       require('../image-targets/target_petrobras.json'),
       require('../image-targets/target-qrcode.json'),
+       require('../image-targets/exemplo.json'),
     ],
   })
   XR8.addCameraPipelineModule(LandingPage.pipelineModule())
